c0c068551611c15a849dea1c9d9854f7422d61aa
Alex V.B <136813525+wi-p@users.noreply.github.com>
2024-04-12 21:22:25 -0300

    Delete blog_include directory

--- blog_include/js/main.js
+++
@@ -1,5 +0,0 @@
-function getWords(){
-    text = words.value;
-    document.getElementById("para").innerHTML += '<div class="divcomment"><p> <b>Comentário: </b> ' + text + ' </p></div>';
-    document.getElementById("words").value = '';
-}
