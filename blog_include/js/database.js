54b9c829da1aff8c1f1db057dda4e320da32d952
Alex V.B <136813525+wi-p@users.noreply.github.com>
2024-04-13 22:29:44 -0300

    Add files via upload

---
+++ blog_include/js/database.js
@@ -0,0 +1,46 @@
+//      DATABASE        //
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+
+function createTable(){
+    db.serialize(() => {
+        db.run("CREATE TABLE Post (title TEXT, content TEXT)");
+        db.run("CREATE TABLE Comment (comm TEXT, post_id INT, FOREIGN KEY (post_id) REFERENCES Post(id))");
+    })
+
+    console.log("Table created");
+}
+
+
+function insertPost(){
+    tit = title.value;
+    cont = content.value;
+
+    db.serialize(() => {
+        db.run("INSERT INTO Post values (?, ?)",[tit, cont]);
+    });
+
+    document.getElementById("title").value = '';
+    document.getElementById("content").value = '';
+
+    console.log("Post added");
+}
+
+function readPost(){
+    db.serialize(() => {
+        db.each("SELECT title, content FROM Post", (err, row) => {
+            console.log(row.title + " " + row.content);
+        });
+    });
+}
+
+
+//createTable();
+//insertPost();
+//readPost();
+//insertValue();
+//readValue();
+
+
